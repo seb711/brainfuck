d8660975859823989b603231acb534d62800bd46
kosakseb <sebastian.kosak@tum.de>
2023-10-24 22:12:59 +0200

    Edited main

--- src/compiler/main.cpp
+++ src/compiler/main.cpp
@@ -13,23 +13,19 @@
 #include "optimizations/patternOptimizer.hpp"
 
 std::string read_code(std::string file_path) {
-    // Open the file
     std::ifstream file(file_path);
 
-    // Check if the file is successfully opened
     if (!file.is_open()) {
         std::cerr << "Failed to open the file: " << file_path << std::endl;
         return "";
     }
 
-    // Read the file content into a string
     std::string file_content;
     std::string line;
     while (std::getline(file, line)) {
         file_content += line;
     }
 
-    // Close the file
     file.close();
 
     file_content.erase(std::remove(file_content.begin(), file_content.end(), '\n'), file_content.cend());
@@ -47,35 +43,19 @@ int main(int argc, char *argv[]) {
     std::string file_path = argv[1];
     std::string prog = read_code(file_path);
 
+    // Parse program into AST
     brainfuck::Parser parser{prog};
 
-    // std::cout << "PARSED" << std::endl;
-
+    // Optimize the program into AST
     brainfuck::Optimizer optimizer{parser.getRoot()};
-
-    // optimizer.getRoot().print();
-
-    // std::cout << "OPTIMIZED" << std::endl << std::endl;
-
-    // Parse the program into AST
     brainfuck::SquashArithmeticOptimizationStep aO = brainfuck::SquashArithmeticOptimizationStep();
     brainfuck::SquashMoveOptimizationStep mO = brainfuck::SquashMoveOptimizationStep();
     brainfuck::LoopPatternOptimizationStep pO = brainfuck::LoopPatternOptimizationStep();
     std::vector<brainfuck::OptimizationStep*> opts{&aO, &mO, &pO};
     optimizer.optimize(opts);
 
-    // optimizer.getRoot().print();
-
-    // std::cout << "COMPILE" << std::endl;
-
+    // Compile AST to ByteCode
     brainfuck::ByteCompiler byteCompiler{optimizer.getRoot()};
-
-    int counter = 0;
-    for (auto i : byteCompiler.ops) {
-        std::cout << std::to_string(counter) << " : ";
-        i.print();
-    }
-
     byteCompiler.exportByteCode(argv[2]);
 
     return 0;
